cb1505d495cc65f87daf446719bb8fac7614d2ef
Jonathan <105410722+Essencesei@users.noreply.github.com>
2023-06-29 17:59:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const router = require("./routes/routes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://649d55453b4966593e127c32--silver-bunny-623e4b.netlify.app", credentials: true }));
 app.use(cookieParser());
 
 app.use("/test", router);
